81c818bd84154e4e84f3d7307844e22e06c31e7c
Jakob Mygind <jmje@nodes.dk>
2020-03-09 12:42:12 +0100

    Move generator in its own file

--- Model Boiler/Classes/Service/Service.swift
+++ Model Boiler/Classes/Service/Service.swift
@@ -8,60 +8,6 @@
 
 import Foundation
 import Cocoa
-import SwiftSyntax
-import SwiftSemantics
-
-class Generator {
-
-    let source: String
-    
-    init(source: String) {
-        self.source = source
-    }
-    
-     var encode: [String] = ["""
-    public func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-    """
-        ]
-        
-        var initStrings: [String] = ["""
-        public init(from decoder: Decoder) throws {
-            let container = try decoder.container(keyedBy: CodingKeys.self)
-        """
-        ]
-        
-        var codingKeys: [String] = ["""
-            enum CodingKeys: String, CodingKey {
-            """
-        ]
-        
-    func addNode(name: String, type: String, isOptional: Bool = false) {
-           encode.append("    try container.encode(\(name), forKey: .\(name))")
-           if isOptional {
-               initStrings.append("    \(name) = try container.decodeIfPresent(\(type).self, forKey: .\(name))")
-           } else {
-               initStrings.append("    \(name) = try container.decode(\(type).self, forKey: .\(name))")
-           }
-           codingKeys.append("    case \(name) = \"\(name)\"")
-       }
-    
-    func generate() throws -> String {
-        var collector = DeclarationCollector()
-        let tree = try SyntaxParser.parse(source: source)
-        tree.walk(&collector)
-
-        for v in collector.variables {
-            addNode(name: v.name, type: v.typeAnnotation!, isOptional: v.typeAnnotation!.contains("?"))
-        }
-
-        encode.append("}\n")
-        initStrings.append("}")
-        codingKeys.append("}\n")
-        
-        return codingKeys.joined(separator: "\n") + encode.joined(separator: "\n") + initStrings.joined(separator: "\n")
-    }
-}
 
 struct Service {
 
